5a199c56ce79f7d73a57e00f3c4df5095d2235d3
Mishal Islam <mishal.islam01@gmail.com>
2014-11-23 17:22:16 +0000

    rakefile

--- app/server.rb
+++ app/server.rb
@@ -2,16 +2,9 @@ require 'sinatra'
 require 'sinatra/partial'
 require 'data_mapper'
 require 'rack-flash'
-
-
-
-env = ENV['RACK_ENV'] || 'development'
-
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+require_relative './data_mapper_setup'
 require_relative './models/peep'
 require_relative './models/user'
-DataMapper.finalize
-DataMapper.auto_upgrade!
 
 set :partial_template_engine, :erb
 set :public_folder, Proc.new { File.join(root, '.', 'public') }
