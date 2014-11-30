1bca835e06c6d1a32f3e455fb10c1f6f3494c91d
Mishal Islam <mishal.islam01@gmail.com>
2014-11-30 13:54:52 +0000

    tests passed

--- spec/listing_peeps_spec.rb
+++ spec/listing_peeps_spec.rb
@@ -6,9 +6,10 @@ feature "User sees peeps" do
 	include Helper
 
 	scenario "when visiting the homepage, see peeps" do
+		create_user
 		Peep.create(:text =>"hello!", :user_id=>1)
 		visit ('/')
-		expect(page).to have_content("hello!")
+		expect(page).to have_content("test(test) => hello")
 	end
 
 	scenario "when signed in a user should be able to write a peep" do
@@ -16,7 +17,7 @@ feature "User sees peeps" do
 		create_user
 		sign_in("test@test.com", "test")
 		expect{add_peep("hello")}.to change{Peep.count}.from(0).to(1)
-		expect(page).to have_content("hello")
+		expect(page).to have_content("test(test) => hello")
 	end
 
 	scenario "when a user is not signed in they should not be able to write a peep" do
